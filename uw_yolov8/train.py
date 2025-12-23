7ee706624107d51bf7143e31c4fccc2305dc0879
bangkorkor <henrikbang01@gmail.com>
2025-12-23 13:07:24 +0100

    ruod fixes

--- uw_yolov8/train.py
+++ uw_yolov8/train.py
@@ -1,35 +1,31 @@
+# train.py
 from ultralytics import YOLO
 
-
-if __name__ == "__main__":
-    
+def main():
     # Load custom model cfg
     model = YOLO("models/uw_yolov8.yaml").load("weights/yolov8s.pt")  # load a pretrained model 
     model.info(verbose=True)
 
     model.train(
-        data="data/FishDataset/fishdataset.yaml",
-        epochs=150,
-        patience=30,                         # early stopping
+        data="data/RUOD_fix/ruod.yaml",
+        epochs=300,
         imgsz=640,
-        batch=64,                    # global batch; DDP splits across GPUs
-        cache="disk", 
-        optimizer="adamw",         # smoother convergence than SGD
-        lr0=0.003,                 # initial learning rate
-        weight_decay=0.0005,       # standard regularization
-        momentum=0.937,            # used if optimizer=SGD
-        cos_lr=True,   
-        device=[0, 1],
-        workers=4,                  # ~1 worker per GPU (start low)
-        # amp=False,     # <— disable AMP self-check
+        batch=32,                    
+        optimizer="SGD",
+        lr0=0.01,
+        momentum=0.937,
+        weight_decay=0.0005,
+        patience=30,
+        amp=False,     # <— disable AMP self-check
+        device=[0],
+        workers=2,
         project="runs_uwyolo",
-        name="fishdata_adamw150_2gpu",
+        name="ruod_0873_sgd300_1gpu",
         seed=0,
     )
 
-   
+
+
+if __name__ == "__main__":
+    main()
     
-
-
-
-
