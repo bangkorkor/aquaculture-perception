859d53e47e1c9909fcf954f13f4c71255dbc9644
bangkorkor <henrikbang01@gmail.com>
2026-03-19 18:29:11 +0100

    uw-yolov8 solaqua_fish training and test

--- yolov8_fasternet-main/train.py
+++ yolov8_fasternet-main/train.py
@@ -59,7 +59,50 @@ TRAIN_PRESETS = {
         "copy_paste": 0.0,
 
         "rect": False,
-    }
+    }, 
+    "solaqua_fish_120e_fair": {
+        "epochs": 120,
+        "imgsz": 640,
+        "batch": 32,
+        "nbs": 64,
+
+        "optimizer": "SGD",
+        "lr0": 0.01,
+        "lrf": 0.01,
+        "momentum": 0.937,
+        "weight_decay": 0.0005,
+
+        "warmup_epochs": 3.0,
+        "warmup_momentum": 0.8,
+        "cos_lr": False,
+
+        "amp": False,
+        "device": [0],
+        "workers": 4,
+        "seed": 0,
+        "deterministic": False,
+        "pretrained": True,
+
+        "fliplr": 0.5,
+        "flipud": 0.0,
+
+        "degrees": 0.0,
+        "translate": 0.0,
+        "scale": 0.0,
+        "shear": 0.0,
+        "perspective": 0.0,
+
+        "hsv_h": 0.0,
+        "hsv_s": 0.0,
+        "hsv_v": 0.0,
+
+        "mosaic": 0.0,
+        "mixup": 0.0,
+        "copy_paste": 0.0,
+
+    
+        "rect": False
+      },
 }
 
 
