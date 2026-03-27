a0eb3f841def0b942d31ef5c05812b3b93bc9c3e
bangkorkor <henribang01@gmail.com>
2026-03-27 16:31:50 +0000

    net_fish_sonar mmdet configs

---
+++ mmdetection/configs/net_fish_sonar/_base_/net_fish_sonar_transformer_schedule.py
@@ -0,0 +1,140 @@
+# Shared net_fish_sonar experiment recipe for fair cross-library comparison
+# pretrained = yes
+# augmentation = explicit and limited
+
+classes = (
+    'fish',
+    'net',
+)
+num_classes = len(classes)
+metainfo = dict(classes=classes)
+
+dataset_type = 'CocoDataset'
+data_root = '../data-processing/sonar/net_fish_sonar/processed/'
+backend_args = None
+
+# Explicit, limited augmentation recipe:
+# - resize to 640
+# - horizontal flip only
+# - no mosaic, mixup, hsv, translate, scale, perspective, etc.
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor')
+    )
+]
+
+train_dataloader = dict(
+    batch_size=32,
+    num_workers=8,
+    persistent_workers=True,
+    pin_memory=True,
+    prefetch_factor=4,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='annotations/train.json',
+        data_prefix=dict(img='images/train/'),
+        filter_cfg=dict(filter_empty_gt=False),
+        pipeline=train_pipeline,
+        backend_args=backend_args,
+    )
+)
+
+val_dataloader = dict(
+    batch_size=8,
+    num_workers=4,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='annotations/val.json',
+        data_prefix=dict(img='images/val/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args,
+    )
+)
+
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='annotations/test.json',
+        data_prefix=dict(img='images/test/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args,
+    )
+)
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'annotations/val.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args,
+)
+
+test_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'annotations/test.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args,
+)
+
+optim_wrapper = dict(
+    type='AmpOptimWrapper',
+    optimizer=dict(type='AdamW', lr=1e-4, weight_decay=1e-4),
+    clip_grad=dict(max_norm=0.1, norm_type=2),
+    paramwise_cfg=dict(custom_keys={'backbone': dict(lr_mult=0.1)}),
+)
+
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=0.1,
+        end_factor=1.0,
+        begin=0,
+        end=3,
+        by_epoch=True),
+    dict(
+        type='LinearLR',
+        start_factor=1.0,
+        end_factor=0.01,
+        begin=3,
+        end=80,
+        by_epoch=True),
+]
+
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=80, val_interval=2)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+randomness = dict(seed=0, deterministic=False)
+
+auto_scale_lr = dict(enable=False, base_batch_size=16)
---
+++ mmdetection/configs/net_fish_sonar/_base_/net_fish_sonar_two_stage_schedule.py
@@ -0,0 +1,146 @@
+# Shared net_fish_sonar experiment recipe for fair cross-library comparison
+# pretrained = yes
+# augmentation = explicit and limited
+# two-stage models = Faster R-CNN / Dynamic R-CNN / similar FPN-based detectors
+
+classes = (
+    'fish',
+    'net',
+)
+num_classes = len(classes)
+metainfo = dict(classes=classes)
+
+dataset_type = 'CocoDataset'
+data_root = '../data-processing/sonar/net_fish_sonar/processed/'
+backend_args = None
+
+# Explicit, limited augmentation recipe:
+# - resize to 640
+# - horizontal flip only
+# - no mosaic, mixup, hsv, translate, scale, perspective, etc.
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs'),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'),
+    ),
+]
+
+# Kept parallel to your RUOD / UATD two-stage setup.
+train_dataloader = dict(
+    batch_size=32,
+    num_workers=8,
+    persistent_workers=True,
+    pin_memory=True,
+    prefetch_factor=4,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='annotations/train.json',
+        data_prefix=dict(img='images/train/'),
+        filter_cfg=dict(filter_empty_gt=False),
+        pipeline=train_pipeline,
+        backend_args=backend_args,
+    ),
+)
+
+val_dataloader = dict(
+    batch_size=4,
+    num_workers=4,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='annotations/val.json',
+        data_prefix=dict(img='images/val/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args,
+    ),
+)
+
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='annotations/test.json',
+        data_prefix=dict(img='images/test/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args,
+    ),
+)
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'annotations/val.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args,
+)
+
+test_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'annotations/test.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args,
+)
+
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='SGD',
+        lr=0.02,
+        momentum=0.9,
+        weight_decay=1e-4,
+    ),
+)
+
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=0.001,
+        by_epoch=False,
+        begin=0,
+        end=500,
+    ),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=80,
+        by_epoch=True,
+        milestones=[53, 73],
+        gamma=0.1,
+    ),
+]
+
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=80, val_interval=2)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+randomness = dict(seed=0, deterministic=False)
+
+auto_scale_lr = dict(enable=False, base_batch_size=16)
