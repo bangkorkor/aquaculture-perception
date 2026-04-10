9feae1704d2a39b28227dd0ebd3e61a354ac56ba
bangkorkor <henrikbang01@gmail.com>
2026-04-10 10:54:19 +0200

    trakcing runs now saves in folder

--- tracking/tracking.py
+++ tracking/tracking.py
@@ -1,12 +1,20 @@
 from ultralytics import YOLO
 
 # Load an official or custom model
-model = YOLO("yolov8n.pt")  # Load an official Detect model
+model = YOLO("../runs/detect/outputs/training/solaqua_fish/rt_detr_solaqua_fish_120e_fair/weights/best.pt")  # Load an official Detect model
 
 
 
 # Perform tracking with the model
 
-results = model.track(source="../data-processing/vision/FishVideo/FishVideo.mp4", show=True, tracker="bytetrack.yaml")  # with ByteTrack
+results = model.track(
+    source="../data-processing/vision/SOLAQUA/raw_processed/mp4s/vision_raw_2024-08-20_13-55-34.mp4",
+    tracker="bytetrack.yaml",
+    show=False,
+    save=True,
+    stream=True,
+    device="cpu", # remove for gpu/automatic
+)
 
-
+for r in results:
+    pass
